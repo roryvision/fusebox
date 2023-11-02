d46d9d734f2356d47764185d9e1895d92963d797
roryvisuals <anrory21@gmail.com>
2023-11-02 14:47:42 -0700

    Initial commit with DB connection

---
+++ src/helpers/db-connection.php
@@ -0,0 +1,32 @@
+<?php
+require_once(__DIR__ . '/../../vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT'] . '/acad276/fusebox/');
+if (!$dotenv->safeLoad()) {
+  echo(".env file not found or could not be loaded.");
+}
+
+function openCon(): mysqli {
+  $dbHost = $_ENV["DB_HOST"];
+  $dbUser = $_ENV["DB_USER"];
+  $dbPassword = $_ENV["DB_PASSWORD"];
+  $dbName = $_ENV["DB_NAME"];
+
+  $conn = new mysqli(
+    $dbHost,
+    $dbUser,
+    $dbPassword,
+    $dbName,
+  );
+
+  if ($conn -> connect_error) {
+    die("Connection failed: " . $conn -> connect_error);
+  }
+
+  return $conn;
+}
+
+function closeCon($conn) {
+  $conn -> close();
+}
+?>
---
+++ src/pages/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once(__DIR__ . '/../helpers/db-connection.php');
+
+$conn = openCon();
+echo "Connected to the database successfully.";
+closeCon($conn);
+?>
