236355c33a41be93480bdffa7d5f22fcb40fb9b3
roryvisuals <anrory21@gmail.com>
2023-11-05 23:01:50 -0800

    Change file structure

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once(__DIR__ . '/src/helpers/db-connection.php');
+
+$conn = openCon();
+echo "Connected to the database successfully.";
+closeCon($conn);
+?>
+
+<!DOCTYPE html>
+<html lang='en' dir='ltr'>
+  <head>
+    <meta charset='utf-8'>
+    <title>Fusebox</title>
+    <link rel='stylesheet' href='/src/styles/global.css'>
+  </head>
+
+  <body>
+    <p>index.php will serve as our landing page, AKA what the audience sees if they are not logged in. For the Milestone 2 frontpage, please see our dashboard instead: <a href='./src/pages/dashboard.php'>dashboard.php</a></p>
+  </body>
+</html>
--- src/pages/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once(__DIR__ . '/../helpers/db-connection.php');
-
-$conn = openCon();
-echo "Connected to the database successfully.";
-closeCon($conn);
-?>
