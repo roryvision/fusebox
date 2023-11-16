67b980f5bb9a7316615f9110f8d4706c178dcc5b
roryvisuals <anrory21@gmail.com>
2023-11-15 17:02:09 -0800

    Update CSS for Hifi v1.1

--- src/pages/dashboard.php
+++ src/pages/dashboard.php
@@ -1,6 +1,5 @@
 <?php
 require_once('../helpers/db-connection.php');
-$conn = openCon();
 ?>
 
 <!DOCTYPE html>
@@ -10,8 +9,8 @@ $conn = openCon();
     <title>Fusebox</title>
     <link rel='stylesheet' href='../styles/global.css'>
     <link rel='stylesheet' href='../styles/dashboard.css'>
-      <link rel='stylesheet' href='../components/Card/card.css'>
-      <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap' rel='stylesheet'>
+    <link rel='stylesheet' href='../components/Card/card.css'>
+    <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap' rel='stylesheet'>
     <script src='../components/Header/HeaderNav.js' type='text/javascript'></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
@@ -34,24 +33,142 @@ $conn = openCon();
         <li class='cursor-pointer'>projects</li>
         <li class='cursor-pointer'>people</li>
       </ul>
-      <button class='button-basic flex-btwn' id='button-sort'>
-        <img src='../assets/icons/icon_sort.svg' alt='Sort items'>
-        <span>Sort</span>
-      </button>
-    <!--    populating projects-->
-    <div id="project-flex-parent">
-        <?php
 
-        $sql = "SELECT project_name, logline, category_name FROM project, category WHERE category.category_id = project.category_id";
-        $results = $conn->query($sql);
+      <br />
 
-        if (!$results) { // see if data is empty -> check for error!
+      <div>
+        <div id='filter-container'>
+          <p>Results: POPULATE</p>
+
+          <h3>Applied Filters</h3>
+          <!-- populate -->
+          <hr />
+
+          <h3>Project Type</h3>
+          <form>
+            <ul>
+              <li><input type='checkbox' id='type-tech' name='type-tech' value='tech' />
+              <label for='type-tech'>Tech</label></li>
+              <li><input type='checkbox' id='type-visual' name='type-visual' value='visual' />
+              <label for='type-visual'>Visual</label></li>
+              <li><input type='checkbox' id='type-music' name='type-music' value='music' />
+              <label for='type-music'>Music</label></li>
+              <li><input type='checkbox' id='type-film' name='type-film' value='film' />
+              <label for='type-film'>Film</label></li>
+              <li><input type='checkbox' id='type-business' name='type-business' value='business' />
+              <label for='type-business'>Business</label></li>
+              <li><input type='checkbox' id='type-other' name='type-other' value='other' />
+              <label for='type-other'>Other</label></li>
+            </ul>
+          </form>
+          <hr />
+
+          <h3>Project Role</h3>
+          <form>
+            <ul>
+              <li><input type='checkbox' id='role-tech' name='role-tech' value='tech' />
+              <label for='role-tech'>Tech</label>
+                <ul>
+                  <li><input type='checkbox' id='role-frontend' name='role-frontend' value='frontend' />
+                  <label for='role-frontend'>Frontend</label></li>
+                  <li><input type='checkbox' id='role-backend' name='role-backend' value='backend' />
+                  <label for='role-backend'>Backend</label></li>
+                  <li><input type='checkbox' id='role-fullstack' name='role-fullstack' value='fullstack' />
+                  <label for='role-fullstack'>Full Stack</label></li>
+                  <li><input type='checkbox' id='role-uiux' name='role-uiux' value='uiux' />
+                  <label for='role-uiux'></label>UI/UX</li>
+                  <li><input type='checkbox' id='role-engineer' name='role-engineer' value='engineer' />
+                  <label for='role-engineer'></label>Engineer</li>
+                  <li><input type='checkbox' id='role-cybersecurity' name='role-cybersecurity' value='cybersecurity' />
+                  <label for='role-cybersecurity'></label>Cybersecurity</li>
+                </ul>
+              </li>
+              <li><input type='checkbox' id='role-visual' name='role-visual' value='visual' />
+              <label for='role-visual'>Visual</label>
+                <ul>
+                  <li><input type='checkbox' id='role-graphic' name='role-graphic' value='graphic' />
+                  <label for='role-graphic'>Graphic Designer</label></li>
+                  <li><input type='checkbox' id='role-artist' name='role-artist' value='artist' />
+                  <label for='role-artist'></label>Artist</li>
+                  <li><input type='checkbox' id='role-photographer' name='role-photographer' value='photographer' />
+                  <label for='role-photographer'>Photographer</label></li>
+                  <li><input type='checkbox' id='role-3d' name='role-3d' value='3d' />
+                  <label for='role-3d'>3D Modeler</label></li>
+                  <li><input type='checkbox' id='role-fashion' name='role-fashion' value='fashion' />
+                  <label for='role-fashion'>Fashion</label></li>
+                </ul>
+              </li>
+              <li><input type='checkbox' id='role-business' name='role-business' value='business' />
+              <label for='role-business'>Business</label>
+                <ul>
+                  <li><input type='checkbox' id='role-marketing' name='role-marketing' value='marketing' />
+                  <label for='role-marketing'>Marketing</label></li>
+                  <li><input type='checkbox' id='role-financial' name='role-financial' value='financial' />
+                  <label for='role-financial'>Financial</label></li>
+                  <li><input type='checkbox' id='role-product' name='role-product' value='product' />
+                  <label for='role-product'>Product Manager</label></li>
+                  <li><input type='checkbox' id='role-project' name='role-project' value='project' />
+                  <label for='role-project'>Project Manager</label></li>
+                  <li><input type='checkbox' id='role-analyst' name='role-analyst' value='analyst' />
+                  <label for='role-analyst'>Analyst</label></li>
+                  <li><input type='checkbox' id='role-pr' name='role-pr' value='pr' />
+                  <label for='role-pr'>Public Relations</label></li>
+                </ul>
+              </li>
+              <li><input type='checkbox' id='role-film' name='role-film' value='film' />
+              <label for='role-film'>Film</label>
+                <ul>
+                  <li><input type='checkbox' id='role-cinematographer' name='role-cinematographer' value='cinematographer' />
+                  <label for='role-cinematographer'>Cinematographer</label></li>
+                  <li><input type='checkbox' id='role-sound' name='role-sound' value='sound' />
+                  <label for='role-sound'>Sound</label></li>
+                  <li><input type='checkbox' id='role-editor' name='role-editor' value='editor' />
+                  <label for='role-editor'>Editor</label></li>
+                  <li><input type='checkbox' id='role-pa' name='role-pa' value='pa' />
+                  <label for='role-pa'>Production Assistant</label></li>
+                </ul>
+              </li>
+              <li><input type='checkbox' id='role-performing' name='role-performing' value='performing' />
+              <label for='role-performing'>Performing</label>
+                <ul>
+                  <li><input type='checkbox' id='role-actor' name='role-actor' value='actor' />
+                  <label for='role-actor'>Actor</label></li>
+                  <li><input type='checkbox' id='role-dancer' name='role-dancer' value='dancer' />
+                  <label for='role-dancer'>Dancer</label></li>
+                  <li><input type='checkbox' id='role-musician' name='role-musician' value='musician' />
+                  <label for='role-musician'>Musician</label></li>
+                </ul>
+              </li>
+              <li><input type='checkbox' id='role-general' name='role-general' value='general' />
+              <label for='role-general'>General</label>
+                <ul>
+                  <li><input type='checkbox' id='role-writer' name='role-writer' value='writer' />
+                  <label for='role-'writer>Writer</label></li>
+                  <li><input type='checkbox' id='role-event' name='role-event' value='event' />
+                  <label for='role-event'>Event Planner</label></li>
+                  <li><input type='checkbox' id='role-other' name='role-other' value='other' />
+                  <label for='role-other'>Other</label></li>
+                </ul>
+              </li>
+            </ul>
+          </form>
+        </div>
+
+        <div id='cards-container'>
+          <?php
+          $conn = openCon();
+
+          $sql = "SELECT project_name, logline, category_name FROM project, category WHERE category.category_id = project.category_id";
+          $results = $conn->query($sql);
+
+          if (!$results) { // see if data is empty -> check for error!
             echo "SQL error: " . $conn->error;
             exit();
-        }
+          }
 
-        while($currentrow = $results->fetch_assoc()) {
+          while($currentrow = $results->fetch_assoc()) {
             echo "<div class='card card-project'>";
+            echo "<img src='../assets/icons/save_overlay.png' alt='Save project' class='card-save'>";
             echo "<p class='category'>" . $currentrow['category_name'] . "</p>";
             echo "<h2>" . $currentrow['project_name'] . "</h2>";
             echo "<p>" . $currentrow['logline'] . "</p>";
@@ -63,13 +180,12 @@ $conn = openCon();
                 <div class='tag w-fit'>Film</div>
                 </div>
                 </div><br/>";
-        }
-        closeCon($conn);
-        ?>
+          }
+
+          closeCon($conn);
+          ?>
         </div>
+      </div>
     </div>
-
-
-
   </body>
 </html>
