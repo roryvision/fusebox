f86f2067504537a05f7378a383558c00d29955b5
laurenggraham <lggraham@usc.edu>
2023-11-21 14:21:51 -0800

    Login Edits

--- src/pages/Security/login/login.php
+++ src/pages/Security/login/login.php
@@ -1,12 +1,11 @@
 <?php
 //require_once(__DIR__ . '/src/helpers/db-connection.php');
 
-
+include ("src/helpers/db-connection.php");
 session_start();
 //idk if this is right
-include ("src/helpers/db-connection.php");
 $conn = openCon();
-echo "Connected to the database successfully.";
+//echo "Connected to the database successfully.";
 
 if(isset($_POST['email']) && isset($_POST['password'])) {
     function validate($data): string
@@ -22,16 +21,16 @@ $password = validate($_POST['password']);
 
 //check to make sure they enter a email & password
 if(empty($email)){
-    header ("Location: index.php?error=Email is required");
+    header ("Location: login.html?error=Email is required.");
     exit();
 }
 else if(empty($password)){
-    header ("Location: index.php?error=Password is required");
+    header ("Location: login.html?error=Password is required.");
     exit();
 }
 
 //need to add "users" to DB
-$sql = "SELECT * FROM users WHERE user_name ='$email' AND password='$password'";
+$sql = "SELECT * FROM profile WHERE email ='$email' AND password='$password'";
 
 //IGNORE cant use this below bc don't have $mysql?
 //$results = $mysql->query($sql);
@@ -44,25 +43,25 @@ if(!$results) {
 }
 
 //checking match with DB
-if(mysqli_num_rows($results) === 1) {
+if(mysqli_num_rows($results) == 1) {
     $currentrow = $results->fetch_assoc();
-    if($currentrow['user_name'] === $email && $currentrow['password'] === $password) {
+    if($currentrow['email'] == $email && $currentrow['password'] == $password) {
         echo "Logged in successful!";
         
-        //attempt at sessions
-        $_SESSION['user_name'] = $currentrow['user_name'];
-        $_SESSION['name'] = $currentrow['name'];
+        //session variables to pull from
+        $_SESSION['email'] = $currentrow['email'];
+        $_SESSION['fname'] = $currentrow['fname'];
+        $_SESSION['lname'] = $currentrow['lname'];
         $_SESSION['id'] = $currentrow['id'];//primary
 
-        //change page location to home via successful login, cahnge this to account page? or projects?
-        header("Location: home.php");
+        header("Location: dashboard.php");
     }
     else {
-        header ("Location: index.php?error=Incorrect Username or Password");
+        header ("Location: login.html?error=Incorrect Username or Password");
     }
 }
 else {
-    header ("Location: index.php");
+    header ("Location: login.html");
 }
 closeCon($conn);
 exit();
