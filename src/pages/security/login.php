68f024f52d9c576c8ffcc2160b12fb0ad9bcad45
laurenggraham <lggraham@usc.edu>
2023-11-27 20:47:23 -0800

    Css for login & Signup

--- src/pages/security/login.php
+++ src/pages/security/login.php
@@ -36,35 +36,100 @@ $is_invalid = true;
 closeCon($conn);
 }
 ?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Login</title>
     <meta charset="UTF-8">
     <link rel='stylesheet' href='../styles/global.css'>
+    <link href="style.css" rel="stylesheet" type="text/css" >
     <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap' rel='stylesheet'>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
 
-<h1>Login</h1>
-<!--check validity, gives the least info-->
-<?php if ($is_invalid): ?>
+<div class="flex">
+    <div class="left_content">
+        <div class="left_text">
+            <h4>The best place to find your dream team.</h4>
+            <p>Find the students you need to bring your project to life.</p>
+        </div>
+    </div>
+    <?php if ($is_invalid): ?>
     <em>Invalid login</em>
-<?php endif; ?>
+    <?php endif; ?>
+    <div class="flex">
+        <div class="right_content">
+            <div class="title">
+                <img src="../assets/icons/icon_inbox.svg">
+                <h1>Welcome Back!</h1>
+            </div>
+            <div>
+                <form method="post">
 
-<form method="post">
-    <label for="email">email</label>
-    <input type="email" name="email" id="email"
-<!--           if invalid pw, this keeps email filled in for easy ux-->
-<!--           remembers email when login is redisplayed-->
-           value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+                    <div class="entry">
+                        <label for="email">Email</label><br>
+                        <input type="email" name="email" id="email" placeholder="Email"
+                               value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+                        <!--           if invalid pw, this keeps email filled in for easy ux-->
+                        <!--           remembers email when login is redisplayed-->
+                    </div>
 
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password">
+                    <div class="entry">
+                        <label for="password">Password</label><br>
+                        <input type="password" id="password" name="password"  placeholder="Password">
+                    </div>
 
-    <button>Log in</button>
-</form>
+                    <div class="secondary"><a href="">Forgot Password?</a></div>
+
+                    <div class="button">
+                        <input type="submit" value="Create Account" class="button">
+                    </div>
+
+                </form>
+                <div class="bottom_redirect">
+                    <p>Not on Fusebox yet? <a href="src/pages/security/signup.html">Create an Account</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
+
+<!---->
+<!--<!DOCTYPE html>-->
+<!--<html>-->
+<!--<head>-->
+<!--    <title>Login</title>-->
+<!--    <meta charset="UTF-8">-->
+<!--    <link rel='stylesheet' href='../styles/global.css'>-->
+<!--    <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap' rel='stylesheet'>-->
+<!--    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>-->
+<!--</head>-->
+<!--<body>-->
+<!---->
+<!--<h1>Login</h1>-->
+<!--check validity, gives the least info-->
+<?php //if ($is_invalid): ?>
+<!--    <em>Invalid login</em>-->
+<?php //endif; ?>
+<!---->
+<!--<form method="post">-->
+<!--    <label for="email">email</label>-->
+<!--    <input type="email" name="email" id="email"-->
+<!--        if invalid pw, this keeps email filled in for easy ux-->
+<!--         remembers email when login is redisplayed-->
+<!--           value="--><?php //= htmlspecialchars($_POST["email"] ?? "") ?><!--">-->
+<!---->
+<!--    <label for="password">Password</label>-->
+<!--    <input type="password" name="password" id="password">-->
+<!---->
+<!--    <button>Log in</button>-->
+<!--</form>-->
+<!---->
+<!--</body>-->
+<!--</html>-->
