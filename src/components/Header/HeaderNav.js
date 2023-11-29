f124e6f22f8d13aa31482508e31bc8668a1f5a2f
faithyeji <faithyejiseo@gmail.com>
2023-11-29 14:16:15 -0800

    remove inbox from nav

--- src/components/Header/HeaderNav.js
+++ src/components/Header/HeaderNav.js
@@ -12,7 +12,7 @@ headerTemplate.innerHTML = `
       </li>
       <div class='flex-btwn'>
         <li><a href='../pages/createProject.html'><button class='button-basic cursor-pointer'>Create a Project</button></a></li>
-        <li><img src='../assets/icons/icon_inbox.svg' alt='Inbox' class='button-nav cursor-pointer' /></li>
+<!--        <li><img src='../assets/icons/icon_inbox.svg' alt='Inbox' class='button-nav cursor-pointer' /></li>-->
         <li><a href='../pages/profile.html'><img src='../assets/icons/icon_profile.svg' alt='Profile' class='button-nav cursor-pointer' /></a></li>
       </div>
     </ul>
