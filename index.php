d5ba490ac41719fc7387725336186fdf2a2c8494
faithyeji <faithyejiseo@gmail.com>
2023-12-04 04:36:16 -0800

    fixed index.php with new masthead

--- index.php
+++ index.php
@@ -7,17 +7,20 @@ session_start();
 
 //check for user_id value, if it is set,
 if (isset($_SESSION["user_id"])) {
+    header("Location: /acad276/fusebox/src/pages/dashboard.php");
+    exit();
+
 //then retrieve user record from db
-  $conn = openCon();
-//sql to select user from db
-  $sql = "SELECT * FROM profile
-          WHERE id = {$_SESSION["user_id"]}";
-
-  $result = $conn->query($sql);
-//associative array with record values
-  $user = $result->fetch_assoc();
-
-  closeCon($conn);
+//  $conn = openCon();
+////sql to select user from db
+//  $sql = "SELECT * FROM profile
+//          WHERE id = {$_SESSION["user_id"]}";
+//
+//  $result = $conn->query($sql);
+////associative array with record values
+//  $user = $result->fetch_assoc();
+//
+//  closeCon($conn);
 }
 ?>
 <!DOCTYPE html>
@@ -25,7 +28,7 @@ if (isset($_SESSION["user_id"])) {
     <head>
       <title>Home</title>
       <meta charset="UTF-8">
-      <meta name="description" content="description of page goes here">
+      <meta name="description" content="Fusebox: connecting to create.">
 <meta name="keywords" content="collaboration job board hiring project film open source">
       <link rel="stylesheet" href="src/styles/global.css" type="text/css">
       <link rel='stylesheet' href='src/components/Header/header.css'>
@@ -49,7 +52,7 @@ if (isset($_SESSION["user_id"])) {
         <div class="container">
             <header id='header' class='w-100'>
                 <ul class='flex-row-btwn-wrap'>
-                    <li><a href='src/pages/dashboard.php'><img src='src/assets/images/masthead.png' alt='Fusebox masthead' class='cursor-pointer' style='height: 50px;' /></a></li>
+                    <li><a href='/acad276/fusebox/src/pages/dashboard.php'><img src='/acad276/fusebox/src/assets/images/masthead.svg' alt='Fusebox masthead' class='cursor-pointer' id='header-logo' style='height: 30px; padding-top:2px;' /></a></li>
                     <div class='flex-row-btwn-wrap'>
                         <li><a href="src/pages/security/login.php"><button class='button cursor-pointer'>Log in</button></a></li>
                         <li><a href="src/pages/security/signup.html"><button class='button-red cursor-pointer'>Sign up</button></a></li>
