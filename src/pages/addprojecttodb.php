a709322f7be601f7b108f3182216c25a22093574
parislittle <129906401+parislittle@users.noreply.github.com>
2023-12-03 21:06:34 -0800

    Update addprojecttodb.php

--- src/pages/addprojecttodb.php
+++ src/pages/addprojecttodb.php
@@ -1,12 +1,11 @@
 <?php
+require_once('../helpers/db-connection.php');
 $conn = openCon();
 session_start();
 if($conn->connect_errno) {
     echo "db connection error : " . $conn->connect_error;
     exit();
 }
-require_once('../helpers/db-connection.php');
-
 if (isset($_REQUEST["selectedRoles"]) && is_array($_REQUEST["selectedRoles"])) {
     $selectedRoles = implode(', ', $_REQUEST["selectedRoles"]);
 } else {
@@ -17,9 +16,16 @@ $sql = "INSERT INTO project (project_name, logline, description, category_id, ro
         VALUES ('" . $_REQUEST["projectname"] . "', '" . $_REQUEST["logline"] . "', '" . $_REQUEST["description"] . "', '" . $_REQUEST["selectedCategory"] . "', '" . $selectedRoles . "')";
 
 
-$rolesql = "INSERT INTO projects_x_roles
-(role_id)
-VALUES ('" . $_REQUEST["selectedRoles"] . "')";
+$selectedRoles = isset($_REQUEST["selectedRoles"]) ? $_REQUEST["selectedRoles"] : [];
+
+foreach ($selectedRoles as $roleId) {
+    $sqlRoles = "INSERT INTO projects_x_roles (project_id, role_id) VALUES ('$projectId', '$roleId')";
+
+    if ($conn->query($sqlRoles) !== TRUE) {
+        echo "Error: " . $sqlRoles . "<br>" . $conn->error;
+        exit();
+    }
+}
 
 $results = $conn->query($sql);
 
