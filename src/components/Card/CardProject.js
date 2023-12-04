ed2df7b4ed0e8ce38f8cfa2d957a01d919d172ea
faithyeji <faithyejiseo@gmail.com>
2023-12-04 04:50:16 -0800

    hover color change for project cards

--- src/components/Card/CardProject.js
+++ src/components/Card/CardProject.js
@@ -71,8 +71,18 @@ class CardProject extends HTMLElement {
           break;
       }
 
+      let cardProject = shadow.querySelector('.card-project');
+
       shadow.querySelector('h2').addEventListener('click', () => {
         window.location.href = `project/${encodeURIComponent(p.project_id)}`;
+      });
+      shadow.querySelector('h2').addEventListener('mouseover', () => {
+        cardProject.style.backgroundColor = '#E0D9CC';
+      });
+
+      shadow.querySelector('h2').addEventListener('mouseout', () => {
+        // Replace 'oldColor' with the original color of the card
+        cardProject.style.backgroundColor = '#F0EBE2';
       });
     }
   }
