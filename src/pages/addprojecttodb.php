89da0fe53b112bd4d33863f3ce4a180d2e987f75
parislittle <129906401+parislittle@users.noreply.github.com>
2023-12-03 21:58:48 -0800

    Update addprojecttodb.php

--- src/pages/addprojecttodb.php
+++ src/pages/addprojecttodb.php
@@ -14,7 +14,7 @@ if (isset($_REQUEST["selectedRoles"]) && is_array($_REQUEST["selectedRoles"])) {
 
 
 $sql = "INSERT INTO project (project_name, logline, description, creator_id, category_id) 
-        VALUES ('" . $_REQUEST["projectname"] . "', '" . $_REQUEST["logline"] . "', '" . $_REQUEST["description"] . "', '" . $currentrow['project_id'] . "', '" .$_REQUEST["selectedCategory"] . "')";
+        VALUES ('" . $_REQUEST["projectname"] . "', '" . $_REQUEST["logline"] . "', '" . $_REQUEST["description"] . "', '" . ($_SESSION["user_id"]) . "', '" .$_REQUEST["selectedCategory"] . "')";
 
 if ($conn->query($sql) !== TRUE) {
     echo "Error: " . $sql . "<br>" . $conn->error;
@@ -39,8 +39,31 @@ $results = $conn->query($sql);
 
 
 if(!$results){
-echo "DATABASE ERROR: " . $conn->error;
-exit();
+    echo "DATABASE ERROR: " . $conn->error;
+    exit();
 }
 
 ?>
+
+<html>
+<head>
+    <meta charset='utf-8'>
+    <title>Fusebox</title>
+    <link rel='stylesheet' href='../styles/global.css'>
+    <link rel='stylesheet' href='../styles/dashboard.css'>
+    <link rel='stylesheet' href='../components/Card/card.css'>
+    <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap' rel='stylesheet'>
+    <script src='../components/Header/HeaderNav.js' type='text/javascript'></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+
+<div class="outer">
+    <div id='container'>
+        <header-nav></header-nav>
+        <?php
+        echo "You have succesfully created $_REQUEST[projectname] !"
+        ?>
+    </div>
+</div>
+</html>
+
