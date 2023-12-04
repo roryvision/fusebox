280073eea3f1da59adf5a789b166b9821f9c31a6
faithyeji <faithyejiseo@gmail.com>
2023-12-03 22:49:50 -0800

    Add test check for email sending

--- src/pages/project/email.php
+++ src/pages/project/email.php
@@ -4,7 +4,9 @@ if (!empty($_REQUEST["creator-email"])) {
     $subject = "Someone applied to your Fusebox project!";
     $message = $_REQUEST["email-message"];
     $test = mail($to, $subject, $message);
-    echo("You emailed " . $message);
+    if ($test==1){
+        echo("You emailed " . $message . " to " . $to);
+    }
 }
 ?>
 
